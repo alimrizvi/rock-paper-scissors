ee2ba549fec08ea62cb43dad75fc2fc653483aaa
alimrizvi <62912143+alimrizvi@users.noreply.github.com>
2025-03-09 15:25:26 +0500

    Build UI for rock-paper-scissor game

--- script.js
+++ script.js
@@ -1,44 +1,74 @@
 function getComputerChoice() {
     let randomPick = Math.random() * 3;
-    if (randomPick <= 1) {return "rock"}
-    else if (randomPick > 1 && randomPick <= 2) {return "paper"}
-    else {return "scissor"};
+    if (randomPick <= 1) {return "Rock"}
+    else if (randomPick > 1 && randomPick <= 2) {return "Paper"}
+    else {return "Scissor"};
 }
 
-function getHumanChoice() {
-    let choice = prompt("Enter you choice between a Rock, Paper or Scissor!");
-    return choice.toLowerCase();
-}
-
-function playRound() {
-    const humanChoice = getHumanChoice()
+function playRound(e) {
+    if (completed==1) {
+        matchWin.remove()
+        completed=0
+        humanScore=0
+        computerScore=0
+    }
+    const humanChoice = e.target.textContent
     const computerChoice = getComputerChoice()
     if (humanChoice == computerChoice) {
-        console.log('Draw! You both picked ' + humanChoice)
+        result = 'Draw! You both picked ' + humanChoice
     }
-    else if ((humanChoice=='rock' && computerChoice=='paper')||(humanChoice=='paper' && computerChoice=='scissor')||(humanChoice=='scissor' && computerChoice=='rock')) {
-        console.log('You Lost! '+ computerChoice + ' beats '  + humanChoice)
-        computerScore+=1
+    else if ((humanChoice=='Rock' && computerChoice=='Paper')||(humanChoice=='Paper' && computerChoice=='Scissor')||(humanChoice=='Scissor' && computerChoice=='Rock')) {
+        result = 'You Lost! '+ computerChoice + ' beats '  + humanChoice
+        console.log(result)
     }
-    else if ((humanChoice=='rock' && computerChoice=='scissor')||(humanChoice=='paper' && computerChoice=='rock')||(humanChoice=='scissor' && computerChoice=='paper')) {
-        console.log('You Win! '+ humanChoice + ' beats '  + computerChoice)
-        humanScore+=1
+    else if ((humanChoice=='Rock' && computerChoice=='Paper')||(humanChoice=='Paper' && computerChoice=='Rock')||(humanChoice=='Scissor' && computerChoice=='Paper')) {
+        result = ('You Won! '+ humanChoice + ' beats '  + computerChoice)
+        console.log(result)
     }
+    updateScore(result)
+}
+
+const options = ['Rock','Paper','Scissor']
+const buttons = []
+const buttonContainer = document.querySelector('#button-container')
+const scoreContainer = document.querySelector('#score-container')
+
+selectButtonText = document.createTextNode("Click on your Pick -> ") 
+buttonContainer.appendChild(selectButtonText)
+
+for (i=0;i<options.length;i++) {
+    buttons[i] = document.createElement('button')
+    buttons[i].textContent = options[i]
+    buttons[i].addEventListener("click",playRound)
+    buttonContainer.appendChild(buttons[i])
 }
 
 let humanScore = 0;
 let computerScore = 0;
+let completed = 0;
+roundResult = document.createElement('p')
+scoreContainer.appendChild(roundResult)
+gameScore = document.createElement('p')
+scoreContainer.appendChild(gameScore)
+matchWin = document.createElement('p')
+reMatchMessage = document.createTextNode('') 
+matchWin.appendChild(reMatchMessage)
+scoreContainer.appendChild(matchWin)
 
-function playGame() {
-    playRound()
-    playRound()
-    playRound()
-    playRound()
-    playRound()
-    if (humanScore>computerScore) {console.log('Human Wins '+ humanScore + ':'+computerScore)}
-    else if (humanScore==computerScore) {console.log('Both Draw '+ humanScore + ':'+computerScore)}
-    else if (humanScore<computerScore) {console.log('Computer Wins '+ computerScore + ':'+humanScore)};
+
+function updateScore(result) {
+    roundResult.textContent = result
+    gameScore.textContent = 'Human | ' + humanScore + ' - ' + computerScore + ' | Computer'
+    if (humanScore==5|computerScore==5) {
+        if (humanScore>computerScore) {matchResult = 'Human Wins '+ humanScore + ' - '+computerScore}
+        else if (humanScore<computerScore) {matchResult ='Computer Wins '+ computerScore + ' - '+humanScore};
+        completed = 1
+        matchWin = document.createElement('p')
+        reMatchMessage = document.createTextNode(' -- Pick again to rematch!')
+        matchWin.textContent = matchResult 
+        matchWin.appendChild(reMatchMessage)
+        scoreContainer.appendChild(matchWin)
+    }
 }
 
-playGame()
 
